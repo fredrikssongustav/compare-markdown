bdfdf72a847c0d2ebc15a2de55460a83efb1a126
Gustav Fredriksson <gustav.fredriksson@tink.se>
2020-05-02 18:09:26 +0200

    feat: first firebase api call -> create source

---
+++ server/api.js
@@ -0,0 +1,33 @@
+const fetch = require("node-fetch");
+const uuid = require('uuid')
+
+const API_URL = `https://${process.env.FIREBASE_ID}.firebaseio.com`;
+
+const log = function(...args) {
+  args.forEach(arg => {
+    console.log(arg);
+  });
+  console.log("\n\n");
+};
+
+const createSource = async (markdown) => {
+  log(markdown)
+  const authId = uuid.v4()
+  const response = await fetch(`${API_URL}/entries.json`, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: `{"markdown": "${markdown}","authId":"${authId}"}`
+  });
+
+  const entry = await response.json();
+  log("Markdown source entry created", entry);
+
+  return {authId,...entry};
+};
+
+module.exports = {
+  log,
+  createSource,
+};
--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const api = require("./api");
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -11,5 +12,11 @@ const app = express();
 const port = 8080;
 
 app.use(bodyParser.json());
+// Create source
+app.post("/markdown", async (req, res) => {
+  const {markdown} = req.body
+  const source = await api.createSource(markdown);
+  res.json(source);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}.`));
